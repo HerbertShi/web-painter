20ecfaac1289ec924dd55a56313d9db0daf16f0d
Herbert Shi <351096578@qq.com>
2013-06-16 00:48:45 +0800

    test definde class in js

---
+++ js/controller/CanvasControl.js
@@ -0,0 +1,11 @@
+﻿//canvas control class
+//el:the element which will append a canvas
+var CanvasControl = function(el){
+	var $canvas = $("<canvas></canvas>");
+
+	$canvas.attr("width", $(el).width());
+	$canvas.attr("height", $(el).height());
+	$canvas.css("cursor", "url('img/black-pen.ico'),auto");//to do 
+
+	
+};
